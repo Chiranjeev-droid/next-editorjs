78cfbd6a2a45b467ce64b482c4254f8b5c2c15d1
Chiranjeev Vishnoi <66114276+Chiranjeev-droid@users.noreply.github.com>
2023-01-31 18:47:10 +0000

    code converted to simple js

--- README.md
+++
@@ -1,3 +0,0 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).
-
-An example of Editor.js integration with React.
--- pages/editor.js
+++ pages/editor.js
@@ -1,41 +1,29 @@
-import React, { useState } from "react"
-import Head from "next/head"
-import dynamic from "next/dynamic"
-import {
-  useSaveCallback,
-  useLoadData,
-  options,
-  useSetData,
-  useClearDataCallback,
-} from "../components/Editor"
+import React, { useState } from 'react';
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import { useSaveCallback, useLoadData, options, useSetData, useClearDataCallback } from '../components/Editor';
 
-const Editor = dynamic<{
-  editorRef: any
-  children?: any
-  data: any
-  options: any
-}>(
-  () =>
-    import("../components/Editor/editor").then((mod) => mod.EditorContainer),
+const Editor = dynamic(
+  () => import('../components/Editor/editor.js').then(mod => mod.EditorContainer),
   { ssr: false }
-)
+);
 
 export default function EditorPage() {
-  const [editor, setEditor] = useState(null)
-
+  const [editor, setEditor] = useState(null);
+  
   // save handler
-  const onSave = useSaveCallback(editor)
+  const onSave = useSaveCallback(editor);
 
   // load data
-  const { data, loading } = useLoadData()
+  const { data, loading } = useLoadData();
 
   // set saved data
-  useSetData(editor, data)
+  useSetData(editor, data);
 
   // clear data callback
-  const clearData = useClearDataCallback(editor)
+  const clearData = useClearDataCallback(editor);
 
-  const disabled = editor === null || loading
+  const disabled = editor === null || loading;
 
   return (
     <div className="container">
@@ -48,30 +36,25 @@ export default function EditorPage() {
         <h1 className="title">
           Example <a href="https://github.com/codex-team/editor.js">EditorJs</a>
         </h1>
+
         <p className="description">
-          Hit TAB on empty line <code>for toolbar</code>, select text for{" "}
-          <code>more options</code>
+          Hit TAB on empty line <code>for toolbar</code>, select text for <code>more options</code>
         </p>
-        <p>
-          <a href="#" onClick={clearData}>
-            Clear data
-          </a>
-        </p>
+
+        <p><a href="#" onClick={clearData}>Clear data</a></p>
         <div className="editorContainer">
-          <Editor editorRef={setEditor} options={options} data={data} />
+          <Editor reInit editorRef={setEditor} options={options} data={data} />
         </div>
-        <button disabled={disabled} type="button" onClick={onSave}>
-          Save &amp; see console &amp; Reload
-        </button>{" "}
+        <button disabled={disabled} type="button" onClick={onSave}>Save &amp; see console &amp; Reload</button>{' '}
       </main>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          NextJS is powered by{" "}
+          NextJS is powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -85,7 +68,6 @@ export default function EditorPage() {
           justify-content: center;
           align-items: center;
         }
-
         main {
           padding: 5rem 0;
           flex: 1;
@@ -94,12 +76,10 @@ export default function EditorPage() {
           justify-content: center;
           align-items: center;
         }
-
         .editorContainer {
           width: 100%;
           margin-bottom: 10px;
         }
-
         footer {
           width: 100%;
           height: 100px;
@@ -108,53 +88,40 @@ export default function EditorPage() {
           justify-content: center;
           align-items: center;
         }
-
         footer img {
           margin-left: 0.5rem;
         }
-
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
-
         a {
           color: inherit;
           text-decoration: none;
         }
-
-        a,
-        .title a {
+        a, .title a {
           color: #0070f3;
           text-decoration: none;
         }
-
-        a:hover,
-        .title a:hover,
-        a:focus,
-        .title a:focus,
-        a:active,
-        .title a:active {
+        a:hover, .title a:hover,
+        a:focus, .title a:focus,
+        a:active, .title a:active {
           text-decoration: underline;
         }
-
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
-
         .title,
         .description {
           text-align: center;
         }
-
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
-
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -163,11 +130,9 @@ export default function EditorPage() {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
-
         .logo {
           height: 1em;
         }
-
         button {
           cursor: pointer;
           color: #fff !important;
@@ -180,17 +145,14 @@ export default function EditorPage() {
           border: none;
           transition: all 0.4s ease 0s;
         }
-
-        button:hover,
-        button:disabled {
+        button:hover, button:disabled {
           background: #434343;
           letter-spacing: 1px;
-          -webkit-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
-          -moz-box-shadow: 0px 5px 40px -10px rgba(0, 0, 0, 0.57);
-          box-shadow: 5px 40px -10px rgba(0, 0, 0, 0.57);
+          -webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+          -moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+          box-shadow: 5px 40px -10px rgba(0,0,0,0.57);
           transition: all 0.4s ease 0s;
         }
-
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -208,7 +170,6 @@ export default function EditorPage() {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
